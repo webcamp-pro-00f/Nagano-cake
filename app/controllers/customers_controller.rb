cddaf83fe65c523197fda8cb9fb0e938606200dc
einbone921 <einbone921@gmail.com>
2020-12-16 14:45:35 +0000

    [Add] customerの詳細、編集画面の追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,17 +1,28 @@
 class CustomersController < ApplicationController
   def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    redirect_to action: :show
   end
 
   def out
   end
 
   def hide
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 
 end
