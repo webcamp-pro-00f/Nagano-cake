d6cbb369d9bf904a3379cabf216718ac098cda0a
saki-smile <sa.su.ko0419@gmail.com>
2020-12-18 02:14:54 +0000

    products-showにカートに追加を設置

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @categories = Category.all
   end
 
   def show
-    @product = Product.find(params[])
+    @categories = Category.all
+    @product = Product.find(params[:id])
   end
-
+  
 end
