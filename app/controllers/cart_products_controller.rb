45b4d5f513dc7f626c19f1ce84ac09aa69a6f8ea
einbone921 <einbone921@gmail.com>
2020-12-19 12:08:13 +0000

    [Update]cart_productコントローラの記述変更

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,12 +1,4 @@
 class CartProductsController < ApplicationController
-  # def create
-  #   @cart_item = CartItem.new(item_params)
-  #   @cart_item.user_id = current_user.id
-  #   #税抜の小計価格を設定
-  #   @cart_item.price = @cart_item.product.price * @cart_item.quantity
-  #   @cart_item.save
-  #   redirect_to cart_items_path
-  # end
 
   def create
     cart_product = CartProduct.new(cart_product_params)
@@ -17,21 +9,29 @@ class CartProductsController < ApplicationController
 
   def index
     @cart_products = current_customer.cart_products
-    # @total_price = @cart_products.product.sum(:price)
   end
 
   def update
+    cart_product = CartProduct.find(params[:id])
+    cart_product.update(cart_product_params)
+    redirect_to cart_products_path
   end
 
   def destroy
+    cart_product = CartProduct.find(params[:id])
+    cart_product.destroy
+    redirect_to cart_products_path
   end
 
   def destroy_all
+    cart_products = current_customer.cart_products
+    cart_products.destroy_all
+    redirect_to cart_products_path
   end
 
   private
 
   def cart_product_params
-    params.require(:cart_product).permit(:customer_id, :product_id, :amount)
+    params.require(:cart_product).permit(:product_id, :customer_id, :amount)
   end
 end
