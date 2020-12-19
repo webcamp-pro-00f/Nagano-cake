d9894c57ec84dccd9d1f31471cc590cd9edf9291
saki-smile <sa.su.ko0419@gmail.com>
2020-12-19 13:48:02 +0000

    products-indexジャンル検索追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
+    if params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @products = @category.products
+    end
   end
+  
 
   def show
     @categories = Category.all
