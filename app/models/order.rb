513a167af3a1c705f6d737f8fd5b43effe855cc9
einbone921 <einbone921@gmail.com>
2020-12-20 07:33:16 +0000

    first commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,6 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum payment_method: {
-<<<<<<< HEAD
-    credit: 0,
-    bank: 1
-  }
-
-=======
   credit: 0,
   bank: 1
   }
@@ -20,5 +14,5 @@ class Order < ApplicationRecord
   "発送準備中": 3,
   "発送済み": 4,
   }
->>>>>>> origin/views
+
 end
