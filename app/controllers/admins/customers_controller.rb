ecbe0917a40bcb9bc0c1a3f84a9be4f83be8b96d
einbone921 <einbone921@gmail.com>
2020-12-20 06:55:49 +0000

    [Add]退会ステータスの追加
    まだ退会した後にカートの中の情報が残っているので、調整します

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -16,10 +16,10 @@ class Admins::CustomersController < ApplicationController
     customer.update(customer_params)
     redirect_to action: :show
   end
-  
+
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number )
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted )
   end
-  
+
 end
