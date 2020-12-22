d4b51999ec775c90f828550cc2022df92c27a6f2
0617yoneda <0617yoneda@gmail.com>
2020-12-22 14:24:18 +0900

    [add]管理者側：注文履歴一覧に東京時刻設定、注文履歴詳細に会員詳細リンク設定・支払方法日本語変換

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,14 @@ class Order < ApplicationRecord
   bank: 1
   }
 
+  def payment_method_text
+    if self.payment_method == "credit"
+      "クレジットカード"
+    elsif self.payment_method == "bank"
+      "銀行振込"
+    end
+  end
+
   enum status: {
   "入金待ち": 0,
   "入金確認": 1,
@@ -15,4 +23,8 @@ class Order < ApplicationRecord
   "発送準備中": 3,
   "発送済み": 4,
   }
+
+  def address_set
+    self.postal_code + self.address
+  end
 end
