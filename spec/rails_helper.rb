e30f1232c680a06cdf8468ed9e52cc1e39dd70c2
0617yoneda <0617yoneda@gmail.com>
2020-12-24 17:46:15 +0900

    []

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 require 'capybara/rspec'
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Capybara::DSL
